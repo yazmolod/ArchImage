2841dddde25f4db5a96d2c9944a2ab6f2a6f05a3
Alexander Litovchenko <yazmolod@gmail.com>
2020-07-24 18:34:59 +0400

    Добавьте файлы проекта.

---
+++ ArchImage/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ArchImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.size_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plusbutton = new System.Windows.Forms.Button();
+            this.minusbutton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // folderButton
+            // 
+            this.folderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.folderButton.Location = new System.Drawing.Point(12, 526);
+            this.folderButton.Name = "folderButton";
+            this.folderButton.Size = new System.Drawing.Size(110, 23);
+            this.folderButton.TabIndex = 0;
+            this.folderButton.Text = "Выбрать папку";
+            this.folderButton.UseVisualStyleBackColor = true;
+            this.folderButton.Click += new System.EventHandler(this.folderButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(450, 450);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // leftButton
+            // 
+            this.leftButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.leftButton.Location = new System.Drawing.Point(12, 468);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(31, 23);
+            this.leftButton.TabIndex = 2;
+            this.leftButton.Text = "<";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.rightButton.Location = new System.Drawing.Point(49, 468);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(31, 23);
+            this.rightButton.TabIndex = 3;
+            this.rightButton.Text = ">";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // size_textBox
+            // 
+            this.size_textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.size_textBox.Location = new System.Drawing.Point(362, 526);
+            this.size_textBox.Name = "size_textBox";
+            this.size_textBox.Size = new System.Drawing.Size(100, 20);
+            this.size_textBox.TabIndex = 4;
+            this.size_textBox.Text = "10";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(362, 507);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Целевой размер, см";
+            // 
+            // plusbutton
+            // 
+            this.plusbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.plusbutton.Location = new System.Drawing.Point(137, 526);
+            this.plusbutton.Name = "plusbutton";
+            this.plusbutton.Size = new System.Drawing.Size(23, 23);
+            this.plusbutton.TabIndex = 6;
+            this.plusbutton.Text = "+";
+            this.plusbutton.UseVisualStyleBackColor = true;
+            this.plusbutton.Click += new System.EventHandler(this.plusbutton_Click);
+            // 
+            // minusbutton
+            // 
+            this.minusbutton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.minusbutton.Location = new System.Drawing.Point(166, 526);
+            this.minusbutton.Name = "minusbutton";
+            this.minusbutton.Size = new System.Drawing.Size(23, 23);
+            this.minusbutton.TabIndex = 7;
+            this.minusbutton.Text = "-";
+            this.minusbutton.UseVisualStyleBackColor = true;
+            this.minusbutton.Click += new System.EventHandler(this.minusbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.minusbutton);
+            this.Controls.Add(this.plusbutton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.size_textBox);
+            this.Controls.Add(this.rightButton);
+            this.Controls.Add(this.leftButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.folderButton);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button folderButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.TextBox size_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button plusbutton;
+        private System.Windows.Forms.Button minusbutton;
+    }
+}
+
---
+++ ArchImage/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ArchImage
+{
+    public partial class Form1 : Form
+    {
+        int _im_index = 0;
+        int CurrentImageIndex {get => _im_index;
+            set 
+            {
+                if (value >= ImagePaths.Count) _im_index = 0;
+                else if (value < 0) _im_index = ImagePaths.Count-1;
+                else _im_index = value;
+            } 
+        }
+        double NeedSize
+        {
+            get
+            {
+                double res;
+                if (double.TryParse(size_textBox.Text, out res))
+                { return res; }
+                else return 1.0;
+            }
+        }
+        int clickCounter = 0;
+        Point pt1;
+        Point pt2;
+        string Folder { get; set; }
+        List<string> ImagePaths { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            BindEvents();
+        }
+
+        private void LoadImagePaths()
+        {
+            DirectoryInfo d = new DirectoryInfo(Folder);
+            ImagePaths = d.GetFiles("*.jpg").Select(x => x.FullName).ToList();
+            if (ImagePaths.Count > 0)
+            {
+                UpdateImage();
+            }
+        }
+
+        private void BindEvents()
+        {
+            pictureBox1.MouseClick += PictureBox1_MouseClick;
+        }
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            double DPI_X, DPI_Y;
+            using (Graphics g = this.CreateGraphics())
+            {
+                DPI_X = g.DpiX;
+                DPI_Y = g.DpiY;
+            }
+            Point coordinates = e.Location;
+            if (clickCounter == 0)
+            {
+                clickCounter += 1;
+                pt1 = coordinates;
+            }
+            else
+            {
+                clickCounter = 0;
+                pt2 = coordinates;
+                double dX = (pt2.X - pt1.X) / DPI_X * 2.54;
+                double dY = (pt2.Y - pt1.Y) / DPI_Y * 2.54;
+                double realDistance = Math.Sqrt(Math.Pow(dX, 2.0) + Math.Pow(dY, 2.0));
+                double factor = NeedSize / realDistance;
+                changeImageSize(factor);
+            }
+        }
+
+        private void changeImageSize(double factor)
+        {
+            Image im = pictureBox1.Image;
+            int new_width = (int)Math.Round(im.Width * factor);
+            int new_height = (int)Math.Round(im.Height * factor);
+            Size size = new Size(new_width, new_height);
+            pictureBox1.Image = new Bitmap(im, size);
+        }
+
+        private void folderButton_Click(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Folder = dialog.SelectedPath;
+                LoadImagePaths();
+            }
+        }
+
+        private void MyForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Left)
+            {
+
+            }
+            else if (e.Modifiers == Keys.Right)
+            {
+
+            }
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            CurrentImageIndex -= 1;
+            UpdateImage();
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            CurrentImageIndex += 1;
+            UpdateImage();
+        }
+
+        private void UpdateImage()
+        {
+            Image i = new Bitmap(ImagePaths[_im_index]);
+            double ow = pictureBox1.Width;
+            double h = i.Height;
+            double w = i.Width;
+            int newHeight = (int)(ow * h / w );
+            Image resize = new Bitmap(i, new Size(pictureBox1.Width, newHeight));
+            pictureBox1.Image = resize;
+        }
+
+        private void plusbutton_Click(object sender, EventArgs e)
+        {
+            changeImageSize(1.25);
+        }
+
+        private void minusbutton_Click(object sender, EventArgs e)
+        {
+            changeImageSize(0.75);
+        }
+    }
+}
